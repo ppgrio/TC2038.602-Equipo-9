9e56fe1ef17c52f231c70c047e272ac158440401
ITESM-VCAM <A01735632@tec.mx>
2026-09-21 23:55:42 -0600

    shuffle array

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
+#include <algorithm>
 #include <array>
 #include <numeric>
+#include <random>
 
 #define BREAK __asm__("int $3")
 
@@ -10,6 +12,10 @@ int main()
     std::array<int, ARRAY_SIZE> int_array;
 
     std::iota(int_array.begin(), int_array.end(), 1);
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::shuffle(int_array.begin(), int_array.end(), gen);
 
     return 0;
 }
